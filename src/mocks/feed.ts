7f49370d2662a36827282282d488022ffdc5beda
fmruiz <x.francoruiz@gmail.com>
2025-01-24 20:18:57 -0300

    feat: add mock feed data

---
+++ src/mocks/feed.ts
@@ -0,0 +1,112 @@
+import { IFeedPost } from "@/types/feed";
+
+export const MOCK_FEED_DATA: IFeedPost[] = [
+  {
+    id: "1",
+    user: { name: "Lucas Larrea", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "2",
+    user: { name: "Eva Lopez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "3",
+    user: { name: "Emma Ramirez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "4",
+    user: { name: "Lucas Larrea", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "5",
+    user: { name: "Eva Lopez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "6",
+    user: { name: "Emma Ramirez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "7",
+    user: { name: "Lucas Larrea", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "8",
+    user: { name: "Eva Lopez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "9",
+    user: { name: "Emma Ramirez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "10",
+    user: { name: "Lucas Larrea", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "11",
+    user: { name: "Eva Lopez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "12",
+    user: { name: "Emma Ramirez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "13",
+    user: { name: "Lucas Larrea", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "14",
+    user: { name: "Eva Lopez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "15",
+    user: { name: "Emma Ramirez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "16",
+    user: { name: "Lucas Larrea", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "17",
+    user: { name: "Eva Lopez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+  {
+    id: "18",
+    user: { name: "Emma Ramirez", image: "avatar" },
+    date: "Hoy",
+    text: "finibus, lectus quis aliquam scelerisque, quam mi venenatis risus, id commodo ex elit in augue. Vestibulum vel tincidunt est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus sit amet auctor dolor, sit amet facilisis felis. In quis tristique sapien. ",
+  },
+];
