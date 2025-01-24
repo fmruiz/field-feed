acd4658188df36e218404ae61a87e232ef725ebf
fmruiz <x.francoruiz@gmail.com>
2025-01-24 20:05:42 -0300

    feat: improve Post component styles

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { IFeedPost } from "@/types/feed";
-import { Avatar } from "./ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Circle, SquarePen, Trash2 } from "lucide-react";
 
 const Post = ({ user, date, text }: IFeedPost) => {
   return (
-    <div className="p-5">
+    <div className="p-5 w-[667px] rounded-md bg-white">
       <div className="flex items-center justify-between mb-4">
-        <Avatar />
+        <div className="flex items-center gap-4">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt={user.name} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div className="flex items-center gap-2">
+            <span className="font-medium text-base text-ff-heading leading-[18px]">
+              {user.name}
+            </span>
+            <Circle width={5} className="text-ff-gray-1" />
+            <span className="font-medium text-ff-secondary text-[10px] leading-4">
+              {date}
+            </span>
+          </div>
+        </div>
+
         <div className="flex items-center gap-2">
-          <span>{user.name}</span>
-          <span>{date}</span>
+          <SquarePen className="text-ff-caption" width={13} />
+          <Trash2 className="text-ff-caption" width={13} />
         </div>
       </div>
-      <p className="text-[14] leading-5">{text}</p>
+      <p className="text-[14] leading-5 text-ff-secondary">{text}</p>
     </div>
   );
 };
