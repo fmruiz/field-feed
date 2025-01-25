277221b597567551f056a7aa89f01ee13916db44
fmruiz <x.francoruiz@gmail.com>
2025-01-24 21:51:24 -0300

    feat: improve feed page

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Post from "@/components/post";
+import Header from "@/components/header";
 import { useContext, useId, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { MOCK_FEED_DATA } from "@/mocks/feed";
 import { GlobalContext } from "@/providers/globalProvider";
 import { IFeedPost } from "@/types/feed";
+import { SquarePen } from "lucide-react";
 
 export default function FeedPage() {
   const [postText, setPostText] = useState<string>("");
@@ -23,7 +25,7 @@ export default function FeedPage() {
         name: username,
         image: "avatar",
       },
-      date: String(Date.now()),
+      date: "today",
       text: "",
     };
 
@@ -35,25 +37,40 @@ export default function FeedPage() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex justify-center h-full flex-col gap-8 row-start-2 items-center sm:items-start bg-slate-400">
-        <div className="flex flex-col gap-[15px]">
-          {postArr.map((feed) => (
-            <Post
-              key={feed.id}
-              id={feed.id}
-              date={feed.date}
-              text={feed.text}
-              user={feed.user}
-            />
-          ))}
+    <div className="flex flex-col items-center justify-center px-[17px] pb-[17px]">
+      <Header />
+      <div className="flex py-[34px] justify-center items-center h-full w-full flex-col gap-[15px] bg-ff-layout rounded-md">
+        <p className="text-xl">
+          Hello, <span className="font-bold">{username}! </span>🚀
+        </p>
 
-          <div>
-            <Textarea onChange={(e) => setPostText(e.target.value)} />
-            <Button onClick={createPostFeed}>Submit</Button>
-          </div>
+        <div className="flex items-start gap-2 mb-8">
+          <label className="sr-only" htmlFor="post-message">
+            Your Post
+          </label>
+          <Textarea
+            cols={70}
+            rows={5}
+            id="post-message"
+            placeholder="Type your message here..."
+            className="bg-white"
+            onChange={(e) => setPostText(e.target.value)}
+          />
+          <Button onClick={createPostFeed}>
+            <SquarePen />
+          </Button>
         </div>
-      </main>
+
+        {postArr.map((feed) => (
+          <Post
+            key={feed.id}
+            id={feed.id}
+            date={feed.date}
+            text={feed.text}
+            user={feed.user}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./icons/logo";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-center h-[83px] w-full">
+      <Logo />
+    </div>
+  );
+};
+
+export default Header;
